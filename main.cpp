437656430622590f28d5cab5245a0ca22f24ba14
niansa <anton-sa@web.de>
2023-04-01 18:11:21 +0200

    Revert "Revert "Extended prompt and filter emojis again""
    This reverts commit 54cc7879d2614a42ef72d7e177f46fb6a796f63a.

--- main.cpp
+++ main.cpp
@@ -147,7 +147,7 @@ class Bot {
                 // Add initial context
                 std::unique_lock L(llm_lock);
                 llm->append("Verlauf des #"+channel.name+" Kanals.\n"
-                            "Notiz 1: "+bot.me.username+" ist ein freundlicher Chatbot, der immer gerne auf deutsch mitredet. Er ist freundlich und hilfsbereit und antwortet immer sofort in einer Nachricht. Er hat guten Humor und mag jeden. Sein Alter ist 16 und er wurde 2007 geboren.\n"
+                            "Notiz 1: "+bot.me.username+" ist ein freundlicher Chatbot, der immer gerne auf deutsch mitredet. Er ist freundlich und hilfsbereit und antwortet immer sofort und ausfuehrlich in einer Nachricht. Er hat guten Humor und mag jeden. Sein Alter ist 16 und er wurde 2007 geboren.\n"
                             "Notiz 2: Ecki heisst in Wirklichkeit Eckhard Kohlhuber und kommt aus Bayern.\n"
                             "\n", cb);
                 // Delete progress message
@@ -167,7 +167,7 @@ class Bot {
             std::unique_lock L(llm_lock);
             for (const auto line : str_split(msg.content, '\n')) {
                 Timer timeout;
-                llm->append(msg.author.username+": "+std::string(line)+'\n', [&] (float) {
+                llm->append(msg.author.username+": "+clean_string(line)+'\n', [&] (float) {
                     if (timeout.get<std::chrono::minutes>() > 1) {
                         std::cerr << "\nWarning: Timeout reached processing message" << std::endl;
                         return false;
